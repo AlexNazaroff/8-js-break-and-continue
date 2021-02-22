ac5f4c4fdbf2d4df215c5f0876074c2eb070bfdc
AlexNazaroff <77842180+AlexNazaroff@users.noreply.github.com>
2021-02-22 12:05:19 +0500

    Add files via upload

---
+++ js8.js
@@ -0,0 +1,19 @@
+//break
+for(var i=0;i<10;i++){
+    console.log('i=' +i);
+    if (i==5) break;
+}
+console.log('                        ');
+//continue
+for( i=0;i<10;i++){
+    
+    if (i % 2==0) continue;
+    console.log('i=' +i);
+}
+//continue
+for( i=0;i<10;i++){
+ console.log('                        ') ;  
+    if (i % 2==0) {
+        console.log('i=' +i);
+    }
+   }
